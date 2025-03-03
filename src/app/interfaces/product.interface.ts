766f6a75f481229cc5954b286f16a255f2bef9c8
KANTNOLI <zahargysev02@gmail.com>
2025-03-03 06:30:14 +0300

    0.0.1 edit visual list

---
+++ src/app/interfaces/product.interface.ts
@@ -0,0 +1,10 @@
+export default interface Product {
+    img: string,
+    title: string,
+    squirrels: number,
+    fats: number,
+    carbohydrates: number,
+    callories: number
+}
+
+// https://static.tildacdn.com/tild6439-6561-4330-b262-653263333332/kazan_category_outsi.jpg
